63fba620c60fdad279a0a145e8d10b3492885cf9
IrelDev <ireldeveloper@gmail.com>
2020-09-19 20:20:37 +0500

    Initial Commit

---
+++ ProperWayToDownloadAnImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProperWayToDownloadAnImage
+//
+//  Created by Kirill Pustovalov on 19.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
