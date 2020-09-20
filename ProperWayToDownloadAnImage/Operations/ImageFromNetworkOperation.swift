9c97e284f767ab565c3e7dd7d4da1b85b94bd2bb
IrelDev <ireldeveloper@gmail.com>
2020-09-20 09:46:49 +0500

    Add ImageFromNetworkOperation class

---
+++ ProperWayToDownloadAnImage/Operations/ImageFromNetworkOperation.swift
@@ -0,0 +1,39 @@
+//
+//  ImageFromNetworkOperation.swift
+//  ProperWayToDownloadAnImage
+//
+//  Created by Kirill Pustovalov on 19.09.2020.
+//
+
+import UIKit
+
+class ImageFromNetworkOperation: AsynchronousOperation {
+    var urlToImage: String
+    let completion: (UIImage) -> Void
+    
+    var urlSessionDataTask: URLSessionDataTask?
+    
+    init(urlToImage: String, completion: @escaping (UIImage) -> Void) {
+        self.urlToImage = urlToImage
+        self.completion = completion
+    }
+    
+    override func main() {
+        guard let url = URL(string: urlToImage) else { return }
+        urlSessionDataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+
+            defer { self.state = .finished }
+            
+            guard !self.isCancelled else { return }
+            
+            guard error == nil, let data = data, let image = UIImage(data: data) else { return }
+            
+            self.completion(image)
+        }
+        urlSessionDataTask?.resume()
+    }
+    override func cancel() {
+        super.cancel()
+        urlSessionDataTask?.cancel()
+    }
+}
